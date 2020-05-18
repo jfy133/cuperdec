a639489a4a5cd119939291f1dc862175a94450b9
jfy133 <jfy133@gmail.com>
2020-05-18 15:44:39 +0200

    Initial commit

---
+++ R/loading.R
@@ -0,0 +1,59 @@
+#' Load OTU table
+#'
+#' This loads a typical taxa table (Samples: columns; Taxa: rows) in TSV format
+#' and standardises some columns, storing the table in the form of a tibble.
+#'
+#' @param x a TSV file
+#'
+#' @export
+load_taxatable <- function(x){
+  table <- readr::read_tsv(x)
+
+  if (ncol < 2)
+    stop("[cuperdec] error: your taxa table requires a minimum of 2 columns only: Taxon, Sample_1")
+
+    dplyr::rename(Taxon = 1)
+}
+
+#' Load database
+#'
+#' This loads a taxon / isolation source database file i.e. first column is
+#' a list of taxa, and the second column is a list of isolation sources.
+#'
+#' Taxon names should match that with the taxa table.
+#'
+#' @param x a two column TSV file
+#'
+#' @export
+load_database <- function(x){
+
+  table <- readr::read_tsv(x) %>%
+
+  if (ncol != 2)
+    stop("[cuperdec] error: your isolation source database requires two columns only: Sample, Isolation Source.")
+
+  table %>% dplyr::rename(Taxon = 1, Isolation_Source = 2)
+}
+
+#' Load metadata table
+#'
+#' This loads a metadata table. This needs to include at minimum two columns.
+#' Note that the 'source' column should indicate
+#'
+#' 1. Sample name - a unique identifier for each sample
+#' 2. Sample source - a grouping ID indicating what 'source' the sample is from. This is used for plotting to separate comparative 'sources' to your own samples.
+#'
+#' @param x a file
+#' @param sample_col a column name specifying which column should be used to specify sample names
+#' @param source_col a column name specifying which group or the source the sample is from
+#'
+#' @export
+load_map <- function(x, sample_col, source_col){
+  table <- readr::read_tsv(x)
+
+  if (ncol(table) < 2)
+    stop("[cuperdec] error: your isolation source database a minimum of two columns: Sample, Sample Source.")
+
+  table %>% dplyr::rename(Sample = sample, Sample_Source = source)
+
+}
