a31aaf6d5020de1f2ba0b9b3c4ec54c7ec1e7a29
James Fellows Yates <jfy133@gmail.com>
2021-04-03 22:30:37 +0200

    Linting

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 # defining global variables
 # ugly solution to avoid magrittr NOTE
-# see http://stackoverflow.com/questions/9439256/how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when
 globalVariables(".")
 
 #' @importFrom rlang .data
